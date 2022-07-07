cef55927bcdebef84b6a1b4654e2b2999330a236
Alex Kurov <Alex>
2022-07-08 00:07:59 +0300

    init(power-implicity): starting a new react project (power-implicity)

---
+++ power-implicity/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import { Slider } from '../src/сomponents/Slider/Slider'
+
+
+function App() {
+  return (
+    <div className="alex">
+      <Slider/>
+    </div>
+  );
+}
+
+export default App;
---
+++ power-implicity/src/сomponents/Slider/Slider.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './slider.scss'
+
+export const Slider = () => {
+  return (
+    <div className='slider'>
+      <h1>Hello world</h1>
+    </div>
+  )
+}
