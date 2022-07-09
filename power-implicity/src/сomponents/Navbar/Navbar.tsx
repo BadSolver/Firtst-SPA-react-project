4ec320fa6ccebb56a512192f04bf485bc3b5976b
Alex Kurov <Alex>
2022-07-09 13:27:55 +0300

    feat(power-implicity): add a new component Navbar with styles

--- power-implicity/src/сomponents/Navbar/Navbar.tsx
+++ power-implicity/src/сomponents/Navbar/Navbar.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import './navbar.scss'
+import navbarLogo from '../assets/navbar_logo.png'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar__container'>
+      <div className='navbar__logo'>
+        <a href=""><img src={navbarLogo}  alt="logo"/></a>
+      </div>
+      <div className='navbar__links'>
+        <ul className='navbar_link'>
+          <a href="">Features</a>
+        </ul>
+        <ul className='navbar_link'>
+          <a href="">Partners</a>
+        </ul>
+        <ul className='navbar_link'>
+          <a href="">Stories</a>
+        </ul>
+      </div>
+      <div className='navbar__dwn-btn'>
+          <button className='navbar_btn'>Download for free</button>
+        </div>
+    </div>
   )
 }
